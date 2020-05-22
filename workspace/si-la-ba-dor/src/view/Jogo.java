2f0ca3451fe552379658395db587a11a9735adfb
magrini <magrinibruna@usp.br>
2020-05-22 11:32:25 -0300

    Feito modelo para tela inicial de Menu.java. Adicionado campo de texto e titulo. Feito modelo para tela de jogo Jogo.java. Adicionado texto de mostrar nivel, nome do jogador, botoes de silaba e silabas selecionadas embaixo

---
+++ workspace/si-la-ba-dor/src/view/Jogo.java
@@ -0,0 +1,102 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Jogo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Jogo frame = new Jogo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Jogo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNivel = new JLabel("Nivel: 1");
+		lblNivel.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblNivel.setBounds(29, 12, 70, 15);
+		contentPane.add(lblNivel);
+		
+		JLabel lblJogadorDelamaro = new JLabel("Jogador: Delamaro");
+		lblJogadorDelamaro.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblJogadorDelamaro.setBounds(248, 5, 163, 28);
+		contentPane.add(lblJogadorDelamaro);
+		
+		JButton btnPro = new JButton("PRO");
+		btnPro.setFont(new Font("Dialog", Font.BOLD, 16));
+		btnPro.setBounds(53, 45, 94, 66);
+		contentPane.add(btnPro);
+		
+		JButton btnBa_1 = new JButton("ÇÃO");
+		btnBa_1.setFont(new Font("Dialog", Font.BOLD, 16));
+		btnBa_1.setBounds(179, 45, 88, 66);
+		contentPane.add(btnBa_1);
+		
+		JButton btnBa = new JButton("BA");
+		btnBa.setFont(new Font("Dialog", Font.BOLD, 16));
+		btnBa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnBa.setBounds(300, 45, 94, 66);
+		contentPane.add(btnBa);
+		
+		JButton btnMa = new JButton("MA");
+		btnMa.setFont(new Font("Dialog", Font.BOLD, 16));
+		btnMa.setBounds(53, 132, 94, 66);
+		contentPane.add(btnMa);
+		
+		JButton btnBa_1_1 = new JButton("CHA");
+		btnBa_1_1.setEnabled(false);
+		btnBa_1_1.setFont(new Font("Dialog", Font.BOLD, 16));
+		btnBa_1_1.setBounds(179, 132, 88, 66);
+		contentPane.add(btnBa_1_1);
+		
+		JButton btnPro_2_1 = new JButton("GRA");
+		btnPro_2_1.setFont(new Font("Dialog", Font.BOLD, 16));
+		btnPro_2_1.setBounds(300, 132, 94, 66);
+		contentPane.add(btnPro_2_1);
+		
+		JLabel label = new JLabel("_ _ _ _ _ _ _ _ _ _ ");
+		label.setFont(new Font("Dialog", Font.BOLD, 40));
+		label.setBounds(52, 210, 359, 48);
+		contentPane.add(label);
+		
+		JLabel lblCha = new JLabel(" C    H    A  ");
+		lblCha.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblCha.setBounds(46, 225, 348, 35);
+		contentPane.add(lblCha);
+	}
+
+}
--- workspace/si-la-ba-dor/src/view/Menu.java
+++ workspace/si-la-ba-dor/src/view/Menu.java
@@ -9,12 +9,16 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class Menu extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private String[] levels = {"Nivel 1", "Nivel 2", "Nivel 3"};
+	private JTextField txtName;
 
 	/**
 	 * Launch the application.
@@ -48,12 +52,29 @@ public class Menu extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnPlay.setBounds(159, 183, 117, 25);
+		btnPlay.setBounds(161, 209, 117, 25);
 		contentPane.add(btnPlay);
 		
 		JComboBox cbLevel = new JComboBox(levels);
-		cbLevel.setBounds(123, 60, 194, 24);
+		cbLevel.setBounds(125, 143, 198, 24);
 		cbLevel.setSelectedIndex(0);
 		contentPane.add(cbLevel);
+		
+		JLabel lblSi = new JLabel("SI-LA-BA-DOR");
+		lblSi.setFont(new Font("Dialog", Font.BOLD, 26));
+		lblSi.setBounds(125, 27, 212, 25);
+		contentPane.add(lblSi);
+		
+		txtName = new JTextField();
+		txtName.setFont(new Font("Dialog", Font.PLAIN, 16));
+		txtName.setToolTipText("Seu nome");
+		txtName.setBounds(135, 106, 168, 25);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblDigiteSeuNome = new JLabel("Digite seu nome");
+		lblDigiteSeuNome.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblDigiteSeuNome.setBounds(147, 75, 166, 30);
+		contentPane.add(lblDigiteSeuNome);
 	}
 }
