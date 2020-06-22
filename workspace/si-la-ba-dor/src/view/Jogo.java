168a4bb4a4963c72a8c3134c36c7f7f2ef9924f9
Isadora <isadoracarolsiebert@gmail.com>
2020-06-22 17:13:58 -0300

    Interface inserida

--- workspace/si-la-ba-dor/src/view/Jogo.java
+++ workspace/si-la-ba-dor/src/view/Jogo.java
@@ -8,9 +8,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Jogo extends JFrame {
 
@@ -36,67 +40,104 @@ public class Jogo extends JFrame {
 	 * Create the frame.
 	 */
 	public Jogo() {
+		JLabel background;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		//setBounds(100, 100, 479, 319);
+		setSize(1280, 720);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		ImageIcon img = new ImageIcon("background_silabador.png");
 		
-		JLabel lblNivel = new JLabel("Nivel: 1");
-		lblNivel.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblNivel.setBounds(29, 12, 70, 15);
+		JLabel lblNivel = new JLabel("NÌvel: 1");
+		lblNivel.setForeground(new Color(0, 0, 128));
+		lblNivel.setFont(new Font("Cooper Black", Font.PLAIN, 20));
+		lblNivel.setBounds(29, 8, 113, 40);
 		contentPane.add(lblNivel);
 		
-		JLabel lblJogadorDelamaro = new JLabel("Jogador: Delamaro");
-		lblJogadorDelamaro.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblJogadorDelamaro.setBounds(248, 5, 163, 28);
+		JLabel lblJogadorDelamaro = new JLabel("Acerte a palavra, Delamaro!");
+		lblJogadorDelamaro.setForeground(new Color(0, 0, 128));
+		lblJogadorDelamaro.setFont(new Font("Cooper Black", Font.PLAIN, 50));
+		lblJogadorDelamaro.setBounds(277, 72, 747, 71);
 		contentPane.add(lblJogadorDelamaro);
 		
 		JButton btnPro = new JButton("PRO");
-		btnPro.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnPro.setBounds(53, 45, 94, 66);
+		btnPro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnPro.setBackground(new Color(0, 0, 128));
+		btnPro.setForeground(new Color(255, 204, 102));
+		btnPro.setFont(new Font("Cooper Black", Font.PLAIN, 40));
+		btnPro.setBounds(178, 199, 141, 89);
 		contentPane.add(btnPro);
 		
-		JButton btnBa_1 = new JButton("√á√ÉO");
-		btnBa_1.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnBa_1.setBounds(179, 45, 88, 66);
+		JButton btnBa_1 = new JButton("«√O");
+		btnBa_1.setBackground(new Color(0, 0, 128));
+		btnBa_1.setForeground(new Color(255, 204, 102));
+		btnBa_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnBa_1.setFont(new Font("Cooper Black", Font.PLAIN, 40));
+		btnBa_1.setBounds(457, 199, 141, 89);
 		contentPane.add(btnBa_1);
 		
 		JButton btnBa = new JButton("BA");
-		btnBa.setFont(new Font("Dialog", Font.BOLD, 16));
+		btnBa.setBackground(new Color(0, 0, 128));
+		btnBa.setForeground(new Color(255, 204, 102));
+		btnBa.setFont(new Font("Cooper Black", Font.PLAIN, 40));
 		btnBa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnBa.setBounds(300, 45, 94, 66);
+		btnBa.setBounds(732, 199, 141, 89);
 		contentPane.add(btnBa);
 		
 		JButton btnMa = new JButton("MA");
-		btnMa.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnMa.setBounds(53, 132, 94, 66);
+		btnMa.setBackground(new Color(0, 0, 128));
+		btnMa.setForeground(new Color(255, 204, 102));
+		btnMa.setFont(new Font("Cooper Black", Font.PLAIN, 40));
+		btnMa.setBounds(308, 381, 150, 89);
 		contentPane.add(btnMa);
 		
 		JButton btnBa_1_1 = new JButton("CHA");
+		btnBa_1_1.setBackground(new Color(0, 0, 128));
+		btnBa_1_1.setForeground(new Color(255, 204, 102));
+		btnBa_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnBa_1_1.setEnabled(false);
-		btnBa_1_1.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnBa_1_1.setBounds(179, 132, 88, 66);
+		btnBa_1_1.setFont(new Font("Cooper Black", Font.PLAIN, 40));
+		btnBa_1_1.setBounds(586, 381, 150, 89);
 		contentPane.add(btnBa_1_1);
 		
 		JButton btnPro_2_1 = new JButton("GRA");
-		btnPro_2_1.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnPro_2_1.setBounds(300, 132, 94, 66);
+		btnPro_2_1.setBackground(new Color(0, 0, 128));
+		btnPro_2_1.setForeground(new Color(255, 204, 102));
+		btnPro_2_1.setFont(new Font("Cooper Black", Font.PLAIN, 40));
+		btnPro_2_1.setBounds(903, 381, 141, 89);
 		contentPane.add(btnPro_2_1);
 		
 		JLabel label = new JLabel("_ _ _ _ _ _ _ _ _ _ ");
-		label.setFont(new Font("Dialog", Font.BOLD, 40));
-		label.setBounds(52, 210, 359, 48);
+		label.setForeground(new Color(0, 0, 128));
+		label.setFont(new Font("Dialog", Font.BOLD, 99));
+		label.setBounds(253, 528, 997, 141);
 		contentPane.add(label);
 		
-		JLabel lblCha = new JLabel(" C    H    A  ");
-		lblCha.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblCha.setBounds(46, 225, 348, 35);
+		JLabel lblCha = new JLabel(" C  H  A  ");
+		lblCha.setForeground(Color.ORANGE);
+		lblCha.setFont(new Font("Cooper Black", Font.PLAIN, 65));
+		lblCha.setBounds(238, 586, 348, 55);
 		contentPane.add(lblCha);
+		
+		background = new JLabel("", img,JLabel.CENTER);
+		background.setBackground(new Color(51, 204, 204));
+		background.setBounds(0, 0, 1262, 673);
+		getContentPane().add(background);
 	}
 
 }
--- workspace/si-la-ba-dor/src/view/Menu.java
+++ workspace/si-la-ba-dor/src/view/Menu.java
@@ -1,10 +1,12 @@
 package view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -12,6 +14,7 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.DefaultComboBoxModel;
 
 public class Menu extends JFrame {
 
@@ -40,41 +43,62 @@ public class Menu extends JFrame {
 	 * Create the frame.
 	 */
 	public Menu() {
+		JLabel background;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(1280, 720);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		ImageIcon img = new ImageIcon("background_menu_silabador.png");
 		
 		JButton btnPlay = new JButton("Jogar");
+		btnPlay.setForeground(new Color(0, 0, 128));
+		btnPlay.setFont(new Font("Cooper Black", Font.PLAIN, 40));
 		btnPlay.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnPlay.setBounds(161, 209, 117, 25);
+		btnPlay.setBounds(606, 539, 194, 66);
 		contentPane.add(btnPlay);
 		
 		JComboBox cbLevel = new JComboBox(levels);
-		cbLevel.setBounds(125, 143, 198, 24);
+		cbLevel.setForeground(new Color(0, 0, 128));
+		cbLevel.setFont(new Font("Cooper Black", Font.BOLD, 30));
+		cbLevel.setBounds(577, 366, 235, 53);
+		cbLevel.setModel(new DefaultComboBoxModel(new String[] {"N\u00EDvel 1", "N\u00EDvel 2", "N\u00EDvel 3"}));
 		cbLevel.setSelectedIndex(0);
 		contentPane.add(cbLevel);
 		
 		JLabel lblSi = new JLabel("SI-LA-BA-DOR");
-		lblSi.setFont(new Font("Dialog", Font.BOLD, 26));
-		lblSi.setBounds(125, 27, 212, 25);
+		lblSi.setForeground(new Color(0, 0, 128));
+		lblSi.setFont(new Font("Cooper Black", Font.BOLD, 80));
+		lblSi.setBounds(318, 13, 685, 191);
 		contentPane.add(lblSi);
 		
 		txtName = new JTextField();
-		txtName.setFont(new Font("Dialog", Font.PLAIN, 16));
+		txtName.setFont(new Font("Cooper Black", Font.BOLD, 40));
 		txtName.setToolTipText("Seu nome");
-		txtName.setBounds(135, 106, 168, 25);
+		txtName.setBounds(555, 285, 305, 45);
 		contentPane.add(txtName);
 		txtName.setColumns(10);
 		
-		JLabel lblDigiteSeuNome = new JLabel("Digite seu nome");
-		lblDigiteSeuNome.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblDigiteSeuNome.setBounds(147, 75, 166, 30);
+		JLabel lblDigiteSeuNome = new JLabel("Digite seu nome:");
+		lblDigiteSeuNome.setForeground(new Color(0, 0, 128));
+		lblDigiteSeuNome.setBackground(new Color(240, 240, 240));
+		lblDigiteSeuNome.setFont(new Font("Cooper Black", Font.BOLD, 30));
+		lblDigiteSeuNome.setBounds(253, 277, 350, 66);
 		contentPane.add(lblDigiteSeuNome);
+		
+		background = new JLabel("", img,JLabel.CENTER);
+		background.setBackground(new Color(51, 204, 204));
+		background.setBounds(0, 0, 1262, 673);
+		getContentPane().add(background);
+		
+		JLabel lblBemVindo = new JLabel("Seja bem-vindo(a)!");
+		lblBemVindo.setForeground(new Color(0, 0, 128));
+		lblBemVindo.setFont(new Font("Cooper Black", Font.BOLD, 40));
+		lblBemVindo.setBounds(425, 216, 435, 72);
+		contentPane.add(lblBemVindo);
 	}
 }
